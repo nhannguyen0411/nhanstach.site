e91253d7cba3c0f49848e0c0380db500a35f738b
Nhan Nguyen <nhan.nguyen@zamo.io>
2026-02-20 12:57:07 +0700

    add ws

--- index-ws.js
+++ index-ws.js
@@ -8,6 +8,33 @@ app.get("/", (_, res) => {
 
 server.on("request", app);
 
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log("Server is running on port 3000");
 });
+
+/** Begin WebSocket */
+const WebSocketServer = require("ws").Server;
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  const numClients = wss.clients.size;
+
+  console.log("Clients connected: ", numClients);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send(`Welcome to my websocket server!`);
+  }
+
+  ws.on("close", () => {
+    wss.broadcast(`Client disconnected: ${numClients}`);
+    console.log("A client has disconnected");
+  });
+});
+
+wss.broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    client.send(data);
+  });
+};
